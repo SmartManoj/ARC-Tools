b2ae11bfeb792ebc3c80390e30e5471925c6aa0a
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-11-16 13:31:56 +0530

    Add helper and initial task implementation
    Introduces helper.py with a generic solve_task function for evaluating ARC tasks and outputting results. Adds task_1.py with a template for solving task '00576224', including basic object detection and grid handling.

---
+++ evaluation_tasks_v1/helper.py
@@ -0,0 +1,30 @@
+from arc_tools.grid import Grid, detect_objects, move_object
+from arc_tools.logger import logger
+from arc_tools.utils import debug_output
+import json
+def solve_task(task_id, task_fn):
+    file = rf'../ARC-AGI/data/evaluation/{task_id}.json'
+    data = json.load(open(file, 'r'))
+    train_data = data['train']
+    test_data = data['test']
+    is_passed = False
+    for task_idx, task in enumerate(train_data, 1):
+        grid = Grid(task['input'])
+        output = task_fn(grid)
+        expected_output = Grid(task['output'])
+        if not output.compare(expected_output):
+            logger.info(f"Train Task {task_idx} failed")
+            debug_output(grid, expected_output, output)
+            break
+        logger.info(f"Train Task {task_idx} passed")
+    else:
+        is_passed = True
+    if is_passed:
+        outputs = []
+        for task_idx, task in enumerate(test_data, 1):
+            output = task_fn(Grid(task['input']))
+            outputs.append({"attempt_1": output, "attempt_2": output})
+            logger.info(f"Test Task {task_idx} output successfully generated")
+        with open('output.json', 'w') as file:
+            json.dump(outputs, file)
+    
